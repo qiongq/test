047742a44152f19ed8eaf45e36fce9867d768a50
qiongq <34232365+qiongq@users.noreply.github.com>
2018-03-28 22:43:57 +0800

    腾讯课堂

--- my-project/src/components/Admin.vue
+++ my-project/src/components/Admin.vue
@@ -1,5 +1,30 @@
 <template>
-	<h1>管理</h1>
+	<div class="row">
+		<div class="col-sm-12 col-md-8">
+			<!--new pizza-->
+		</div>
+		<div class="col-sm-12 col-md-4">
+			<!--品种展示-->
+			<h3 class="text-muted my-5">菜单</h3>
+			<table class="table">
+				<thead>
+					<tr>
+						<th>品种</th>
+						<th>删除</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>榴莲pizza</td>
+						<td>
+							<button class="btn btn-outline-danger btn-sm">&times;</button>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+
+		</div>
+	</div>
 </template>
 <script>
 	export default{
---
+++ my-project/src/components/NewPizza.vue
@@ -0,0 +1,29 @@
+<template>
+    <form action="">
+        <h3 class="text-muted my-5">添加新的Pizza</h3>
+        <div class="form-group row">
+            <label for="" class="col-sm-1">品种</label>
+            <div class="col-sm-11">
+                <input type="text" class="form-control" v-model="newPizza.name"/>
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="" class="col-sm-1">描述</label>
+            <div class="col-sm-11">
+                <textarea rows="5" class="form-control" v-model="newPizza.description"></textarea>
+            </div>
+        </div>
+        <p><strong></strong></p>
+    </form>
+</template>
+
+<script>
+export default {
+  data () {
+      return {
+          newPizza:{}
+      }
+  }
+}
+</script>
+
